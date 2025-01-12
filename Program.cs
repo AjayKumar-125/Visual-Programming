49011b3f6af17bbc9ae037b4145148b6893530e8
Ajay Kumar <107914631+AjayKumar-125@users.noreply.github.com>
2025-01-12 16:00:49 +0500

    Datagrid view

--- Program.cs
+++ Program.cs
@@ -1,57 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace ConsoleApp2_Quiz
+namespace DataGridView
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-
-
-
-            int[] arr = { 20, 30, 10, 50, 40 };
-            Console.WriteLine("Unsorted Array");
-            display(arr);
-            Console.WriteLine("Sorted Array");
-            display(selectionsort(arr));
-            Console.ReadKey();
-
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
-
-        public static int[] selectionsort(int[] arr)
-        {
-            for (int i = 0; i < arr.Length - 1; i++)
-            {
-                int begin = i;
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[j] < arr[begin])
-                    {
-                        begin = j;
-                    }
-                }
-                int temp = arr[i];
-                arr[i] = arr[begin];
-                arr[begin] = temp;
-            }
-            return arr;
-        }
-
-        public static void display(int[] arr)
-        {
-            for (int lcv = 0; lcv < arr.Length; lcv++)
-            {
-                Console.WriteLine(arr[lcv]);
-            }
-
-
-        }
-
-
     }
 }
