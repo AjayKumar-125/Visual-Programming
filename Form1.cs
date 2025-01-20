7697de3f5ee2c6d2582e7b10c86f44ee555ede9d
Ajay Kumar <107914631+AjayKumar-125@users.noreply.github.com>
2025-01-20 09:37:59 +0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp3
+namespace WindowsFormsApp4
 {
     public partial class Form1 : Form
     {
@@ -19,7 +19,41 @@ namespace WindowsFormsApp3
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            BackColor = Color.Blue;
+            //this.WindowState = FormWindowState.Maximized;
+            WindowState = FormWindowState.Maximized;
+        }
 
+        
+
+        private void programAToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            new Form2().Show();
+            this.Hide();
+
+        }
+
+        private void porgramBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form3().Show();
+            this.Hide();
+
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void calculatorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Form5().Show();
         }
     }
 }
